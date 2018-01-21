3257d857024a6ef2719b4d3cd5de845851fe855b
Trevor Blackman <trevor@akates.com>
2018-01-20 20:49:27 -0800

    indexStyle.js

---
+++ indexStyle.js
@@ -0,0 +1,38 @@
+
+export const divStyle = {
+  display: 'grid',
+  gridGap: '10px',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+  gridAutoRows: 'auto',
+};
+
+export const titleStyle = {
+  color: '#EDEDED',
+  fontFamily: 'Oswald',
+  textDecoration: 'none',
+  fontSize: '1.3em'
+
+}
+
+export const imgStyle = {
+  width: '80%',
+  height: '80%',
+  objectFit: 'cover',
+  margin: '20px'
+};
+
+export const logoStyle = {
+  height: 20,
+  objectFit:'scale-down'
+
+}
+
+export const formStyle = {
+  color: '#000000',
+  fontFamily: 'Oswald',
+  textDecoration: 'none',
+  fontSize: '1.3em', 
+  marginLeft:'auto', 
+  marginRight:'auto'
+}
+
