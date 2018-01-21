1967b23c538aeb922535d594706432b96381e5f6
Trevor Blackman <trevor@akates.com>
2018-01-20 20:48:33 -0800

    availableBoards.js

---
+++ availableBoards.js
@@ -0,0 +1,15 @@
+
+export const boards = [
+  { name: 'Bonzer', price: 1000, waveSize: 'Knee', image: './Images/Bonzer/BonzerDis.png', imageLogo: './Images/Bonzer/BonzerLogo.png'},
+  { name: 'Black and White', price: 1200, waveSize: 'Head', image: './Images/BW/BWDis.png', imageLogo: './Images/BW/BWLogo.png'},
+  { name: 'Fever', price: 1000, waveSize: 'Double', image: './Images/Fever/FeverDis.png', imageLogo: './Images/Fever/FeverLogo.png'},
+  { name: 'Peregrine', price: 1200, waveSize: 'Knee', image: './Images/peregrine/PeregrineDis.png', imageLogo: './Images/peregrine/PeregrineLogo.png'},
+  { name: 'Rookie', price: 1000, waveSize: 'Head', image: './Images/Rookie/RookieDis.png', imageLogo: './Images/Rookie/RookieLogo.png'},
+  { name: 'Zeus', price: 1200, waveSize: 'Double', image: './Images/Zeus/ZeusDis.png', imageLogo: './Images/Zeus/ZeusLogo.png'},
+  { name: 'Grabbit', price: 1000, waveSize: 'Knee', image: './Images/Grabbit/GrabbitDis.png', imageLogo: './Images/Grabbit/GrabbitLogo.png'},
+  { name: 'Proton', price: 1200, waveSize: 'Head', image: './Images/Proton/ProtonDis.png', imageLogo: './Images/Proton/ProtonLogo.png',},
+  { name: 'FredRubble', price: 1000, waveSize: 'Double', image: './Images/FredRubble/FredRubbleDis.png',imageLogo: './Images/FredRubble/FredRubbleLogo.png'},
+  { name: 'SemiPro', price: 1200, waveSize: 'Knee', image: './Images/SemiPro12/SemiProDis.png', imageLogo: './Images/SemiPro12/SemiProLogo.png'},
+  { name: 'DFR', price: 1200, waveSize: 'Head', image: './Images/DFR/DFRDis.png', imageLogo: './Images/DFR/DFRLogo.png'},
+  { name: 'T-Low', price: 1200, waveSize: 'Double', image: './Images/T-Low/T-LowDis.png', imageLogo: './Images/T-Low/T-LowLogo.png'}
+];
